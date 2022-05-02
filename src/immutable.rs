94c8756c7229f12189d11a9b73e991cb1345516f
ii887522 <ii887522@gmail.com>
2022-05-02 17:48:27 +0800

    Add Delayed

---
+++ src/immutable.rs
@@ -0,0 +1,25 @@
+use std::ops::Deref;
+
+pub struct Immutable<T> {
+  value: T,
+}
+
+impl<T> Immutable<T> {
+  pub fn new(value: T) -> Self {
+    Self { value }
+  }
+}
+
+impl<T> Deref for Immutable<T> {
+  type Target = T;
+
+  fn deref(&self) -> &Self::Target {
+    &self.value
+  }
+}
+
+impl<T> From<T> for Immutable<T> {
+  fn from(value: T) -> Self {
+    Self { value }
+  }
+}
